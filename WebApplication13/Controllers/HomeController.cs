942378549f2c864d30555e69e7ae55fa233352fd
Mohammed Ahmed Hussien <Mohammed.Hussien@outlook.sa>
2022-03-26 12:39:41 +0300

    initi

--- WebApplication13/Controllers/HomeController.cs
+++ WebApplication13/Controllers/HomeController.cs
@@ -11,43 +11,16 @@ namespace WebApplication13.Controllers
 {
     public class HomeController : ApiController
     {
-        // GET: api/Home
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/Home/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Home
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT: api/Home/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Home/5
-        public void Delete(int id)
-        {
-        }
-
         // api/home/getfiles
         [HttpGet]
         [Route("api/home/getfiles")]
         public HttpResponseMessage GetFiles()
         {
+            // By Mohammed Ahmed Hussien Babiker
           //  return new HttpResponseMessage(HttpStatusCode.OK);
 
             HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
 
-            //var filePath = File.OpenRead("E:/TestApp/WebApplication13/WebApplication13/MyFiles/books.jpg");
             var fileName = HttpContext.Current.Server.MapPath("~/MyFiles/books.jpg");
 
 
